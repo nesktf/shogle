0c50aabceda80055136a63c0a3d2ef7c3428d9b9
nesktf <nesktf@proton.me>
2024-05-14 17:10:31 -0300

    Forgot the skybox shader

---
+++ source/shogle/render/shaders/generic_skybox.cpp
@@ -0,0 +1,61 @@
+#include <shogle/render/shaders/generic_skybox.hpp>
+
+#include <shogle/core/log.hpp>
+
+namespace {
+
+const char* vert_src = R"glsl(
+  #version 330 core
+
+  layout (location = 0) in vec3 att_coords;
+  out vec3 texcoord;
+
+  uniform mat4 proj;
+  uniform mat4 view;
+
+  void main() {
+    vec4 pos = proj * view * vec4(att_coords, 1.0f);
+    gl_Position = vec4(pos.x, pos.y, pos.w, pos.w);
+    texcoord = vec3(att_coords.x, att_coords.y, -att_coords.z);
+  }
+
+)glsl";
+
+const char* frag_src = R"glsl(
+  #version 330 core
+
+  in vec3 texcoord;
+  out vec4 frag_color;
+
+  uniform samplerCube skybox;
+
+  void main() {
+    frag_color = texture(skybox, texcoord);
+  }
+)glsl";
+
+}
+
+namespace ntf::shogle::shaders {
+
+generic_skybox::generic_skybox() {
+  try {
+    gl::shader vert {std::string{vert_src}, gl::shader::type::vertex};
+    vert.compile();
+
+    gl::shader frag {std::string{frag_src}, gl::shader::type::fragment};
+    frag.compile();
+
+    attach_shaders(std::move(vert), std::move(frag));
+  } catch(...) {
+    Log::error("[shaders::generic_skybox] Failed to build shader");
+    throw;
+  }
+
+  _view_unif = uniform_location("view");
+  _proj_unif = uniform_location("proj");
+
+  _cubemap_unif = uniform_location("skybox");
+}
+
+} // namespace ntf::shogle::shaders
---
+++ source/shogle/render/shaders/generic_skybox.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <shogle/render/gl/shader_program.hpp>
+#include <shogle/render/gl/texture.hpp>
+
+namespace ntf::shogle::shaders {
+
+class generic_skybox : public gl::shader_program {
+public:
+  using att_coords = gl::shader_attribute<0, vec3>;
+
+public:
+  generic_skybox();
+
+public:
+  inline generic_skybox& set_view(mat4 view) {
+    set_uniform(_view_unif, view);
+    return *this;
+  }
+
+  inline generic_skybox& set_proj(mat4 proj) {
+    set_uniform(_proj_unif, proj);
+    return *this;
+  }
+
+  inline generic_skybox& bind_cubemap(gl::texture& tex) {
+    tex.bind(_cubemap_sampler);
+    set_uniform(_cubemap_unif, _cubemap_sampler);
+    return *this;
+  }
+
+private:
+  enum: int { _cubemap_sampler = 0 };
+
+  uniform_id _view_unif{}, _proj_unif{};
+  uniform_id _cubemap_unif{};
+};
+
+} // namespace ntf::shogel::shaders
