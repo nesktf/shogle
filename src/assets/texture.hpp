477471b3711f46917b279b59152089ed2758a0d2
nesktf <nesktf@proton.me>
2025-03-14 00:05:02 -0300

    Dynamic image array specialization

--- src/assets/texture.hpp
+++ src/assets/texture.hpp
@@ -56,52 +56,94 @@ public:
 
 NTF_DEFINE_TEMPLATE_CHECKER(image_data);
 
-template<
-  tex_depth_type T,
-  size_t array_size,
-  tex_array_dim_type Dim = extent2d,
-  typename Deleter = allocator_delete<T, std::allocator<T>>
->
-struct image_data_array {
-public:
-  static_assert(array_size > 0, "Array size can't be 0");
+namespace impl {
 
-  using depth_type = T;
-  using dim_type = Dim;
-  using array_type = std::array<unique_array<typename T::underlying_type, Deleter>, array_size>;
+template<typename DepthT, typename DimT, uint32 array_size>
+struct image_array_desc {
+  std::array<r_image_data, array_size> make_descriptor(
+    const DimT& offset = {},
+    uint32 level = 0
+  ) const noexcept {
+    std::array<r_image_data, array_size> out;
+    for (size_t i = 0; i < array_size; ++i) {
+      out[i].texels = this->texels[i].get();
+      out[i].format = this->format;
+      out[i].alignment = this->alignment;
+      out[i].extent = tex_extent_cast(this->dim);
+      out[i].offset = tex_array_offset(offset, i);
+      out[i].layer = static_cast<uint32>(i);
+      out[i].level = level;
+    }
+    return out;
+  };
+
+protected:
+  bool _check_texels(auto&& texels) const {
+    for (const auto&& tex : texels) {
+      if (!tex.has_data()) {
+        return true;
+      }
+    }
+    return false;
+  }
+};
+
+template<typename DepthT, typename DimT>
+struct image_array_desc<DepthT, DimT, 0u> {
+  template<allocator_type<r_image_data> Alloc = std::allocator<r_image_data>>
+  unique_array<r_image_data, allocator_delete<r_image_data, Alloc>> make_descriptor(
+    const DimT& offset = {},
+    uint32 level = 0,
+    Alloc&& alloc = {}
+  ) const SHOGLE_ASSET_NOEXCEPT {
+    auto out = unique_array<r_image_data>::from_allocator(::ntf::uninitialized,
+                                                          this->array_size,
+                                                          std::forward<Alloc>(alloc));
+    SHOGLE_ASSET_THROW_IF(!out.has_data(), "Allocation failure");
+
+    for (size_t i = 0; i < this->array_size; ++i) {
+      std::construct_at(out.get()+i, r_image_data{
+        .texels = this->texels.get()+i*tex_stride<DepthT>(this->dim),
+        .format = this->format,
+        .alignment = this->alignment,
+        .extent = tex_extent_cast(this->dim),
+        .offset = tex_array_offset(offset, i),
+        .layer = static_cast<uint32>(i),
+        .level = level,
+      });
+    }
+
+    return out;
+  }
+
+protected:
+  bool _check_texels(auto&& texels, size_t array_size) const {
+    return !texels.has_data() || array_size == 0;
+  }
+};
+
+} // namespace impl
+
+template<
+  tex_depth_type DepthT,
+  size_t array_size,
+  tex_array_dim_type DimT = extent2d,
+  typename Deleter = allocator_delete<DepthT, std::allocator<DepthT>>
+>
+struct image_data_array :
+  public impl::image_array_desc<DepthT, DimT, array_size> {
+public:
+  using depth_type = DepthT;
+  using dim_type = DimT;
+  using array_type =
+    std::array<unique_array<typename DepthT::underlying_type, Deleter>, array_size>;
 
 public:
   image_data_array(array_type&& texels_, dim_type dim_,
                    r_texture_format format_, r_image_alignment alignment_) SHOGLE_ASSET_NOEXCEPT :
     texels{std::move(texels_)}, dim{dim_}, format{format_}, alignment{alignment_}
   {
-    SHOGLE_ASSET_THROW_IF([this](){
-      for (const auto& tex : texels) {
-        if (!tex.has_data()) {
-          return true;
-        }
-      }
-      return false;
-    }(), "No data in texel array");
-  }
-
-public:
-  auto make_descriptor(
-    const dim_type& offset = {},
-    uint32 level = 0
-  ) const noexcept -> std::array<r_image_data, array_size>
-  {
-    std::array<r_image_data, array_size> out;
-    for (size_t i = 0; i < array_size; ++i) {
-      out[i].texels = texels[i].get();
-      out[i].format = format;
-      out[i].alignment = alignment;
-      out[i].extent = tex_extent_cast(dim);
-      out[i].offset = tex_array_offset(offset, i);
-      out[i].layer = static_cast<uint32>(i);
-      out[i].level = level;
-    }
-    return out;
+    SHOGLE_ASSET_THROW_IF(this->_check_texels(texels), "Invalid texel data");
   }
 
 public:
@@ -111,62 +153,96 @@ public:
   r_image_alignment alignment;
 };
 
-NTF_DEFINE_TEMPLATE_CHECKER(image_data_array);
-
-template<typename T, typename Deleter = allocator_delete<T, std::allocator<T>>>
-using cubemap_image_data = image_data_array<T, 6u, uvec2, Deleter>;
-
 template<
-  tex_depth_type T,
-  tex_array_dim_type Dim = extent2d,
-  typename Deleter = allocator_delete<T, std::allocator<T>>
+  tex_depth_type DepthT,
+  tex_array_dim_type DimT,
+  typename Deleter
 >
-struct dense_image_data_array {
+struct image_data_array<DepthT, 0u, DimT, Deleter> :
+  public impl::image_array_desc<DepthT, DimT, 0u> {
 public:
-  using depth_type = T;
-  using dim_type = Dim;
-  using array_type = unique_array<typename T::underlying_type, Deleter>;
+  using depth_type = DepthT;
+  using dim_type = DimT;
+  using array_type =
+    unique_array<unique_array<typename DepthT::underlying_type, Deleter>>;
+
+public:
+  image_data_array(array_type&& texels_, dim_type dim_, size_t array_size_,
+                   r_texture_format format_, r_image_alignment alignment_) SHOGLE_ASSET_NOEXCEPT :
+    texels{std::move(texels_)}, dim{dim_}, array_size{array_size_},
+    format{format_}, alignment{alignment_}
+  {
+    SHOGLE_ASSET_THROW_IF(this->_check_texels(texels, array_size), "Invalid texel data");
+  }
   
 public:
-  dense_image_data_array(array_type&& texels_, size_t array_size_, dim_type dim_,
+  array_type texels;
+  dim_type dim;
+  size_t array_size;
+  r_texture_format format;
+  r_image_alignment alignment;
+};
+
+NTF_DEFINE_TEMPLATE_CHECKER(image_data_array);
+
+template<typename DepthT, typename Deleter = allocator_delete<DepthT, std::allocator<DepthT>>>
+using cubemap_image_data = image_data_array<DepthT, 6u, uvec2, Deleter>;
+
+template<
+  tex_depth_type DepthT,
+  uint32 array_size,
+  tex_array_dim_type DimT = extent2d,
+  typename Deleter = allocator_delete<DepthT, std::allocator<DepthT>>
+>
+struct dense_image_data_array :
+  public impl::image_array_desc<DepthT, DimT, array_size> {
+public:
+  using depth_type = DepthT;
+  using dim_type = DimT;
+  using array_type = unique_array<typename DepthT::underlying_type, Deleter>;
+  
+public:
+  dense_image_data_array(array_type&& texels_, dim_type dim_,
+                         r_texture_format format_,
+                         r_image_alignment alignment_) SHOGLE_ASSET_NOEXCEPT :
+    texels{std::move(texels_)}, dim{dim_}, format{format_}, alignment{alignment_}
+  {
+    SHOGLE_ASSET_THROW_IF(this->_check_texels(texels), "Invalid texel data");
+  }
+  
+public:
+  array_type texels;
+  dim_type dim;
+  r_texture_format format;
+  r_image_alignment alignment;
+};
+
+template<
+  tex_depth_type DepthT,
+  tex_array_dim_type DimT,
+  typename Deleter
+>
+struct dense_image_data_array<DepthT, 0u, DimT, Deleter> :
+  public impl::image_array_desc<DepthT, DimT, 0u> {
+public:
+  using depth_type = DepthT;
+  using dim_type = DimT;
+  using array_type = unique_array<typename DepthT::underlying_type, Deleter>;
+
+public:
+  dense_image_data_array(array_type&& texels_, dim_type dim_, size_t array_size_,
                          r_texture_format format_,
                          r_image_alignment alignment_) SHOGLE_ASSET_NOEXCEPT :
     texels{std::move(texels_)}, array_size{array_size_}, dim{dim_},
     format{format_}, alignment{alignment_}
   {
-    SHOGLE_ASSET_THROW_IF(!texels.has_data(), "No data in texel array");
-    SHOGLE_ASSET_THROW_IF(array_size == 0, "Array size can't be 0");
+    SHOGLE_ASSET_THROW_IF(this->_check_texels(texels, array_size), "Invalid texel data");
   }
 
-public:
-  template<allocator_type<r_image_data> Alloc = std::allocator<r_image_data>>
-  auto make_descriptor(
-    const dim_type& offset = {},
-    uint32 level = 0,
-    Alloc&& alloc = {}
-  ) const SHOGLE_ASSET_NOEXCEPT -> unique_array<r_image_data, allocator_delete<T, Alloc>>
-  {
-    auto out = unique_array<r_image_data>::from_allocator(::ntf::uninitialized, array_size, alloc);
-    SHOGLE_ASSET_THROW_IF(!out.has_data(), "Allocation failure");
-
-    for (size_t i = 0; i < array_size; ++i) {
-      std::construct_at(out.get()+i, r_image_data{
-        .texels = texels.get()+i*tex_stride<T>(dim),
-        .format = format,
-        .alignment = alignment,
-        .extent = tex_extent_cast(dim),
-        .offset = tex_array_offset(offset, i),
-        .layer = static_cast<uint32>(i),
-        .level = level,
-      });
-    }
-    return out;
-  }
-  
 public:
   array_type texels;
-  size_t array_size;
   dim_type dim;
+  size_t array_size;
   r_texture_format format;
   r_image_alignment alignment;
 };
